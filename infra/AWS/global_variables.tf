0dfae154bc673b3d85121024d73e40f9b077eaab
dhamodarankms <dhamodarankms@gmail.com>
2023-05-18 09:36:55 +1200

    Public AWS S3 infra

---
+++ infra/AWS/global_variables.tf
@@ -0,0 +1,7 @@
+variable "access_key" {
+    default = "AKIA2AVSWAOFI26HM3LW"
+}
+
+variable "secret_key" {
+    default = "RCNo+6QtFWUxQDiCOOJK7rehnR6yKhRQ6GV2v6bv"
+}
---
+++ infra/AWS/set_up_aws_s3_bucket.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    access_key = var.access_key
+    secret_key = var.secret_key
+    region = "ap-southeast-2"
+}
+
+resource "aws_s3_bucket" "DataAnalytics" {
+    bucket = "sdgdatalakesecond"
+    tags = {
+        name = "SDG"
+        value = "databricks"
+    }
+}
