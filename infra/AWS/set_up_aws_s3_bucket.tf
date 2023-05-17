005191fbbc4cd0f94775e61044f7f9f8a15076e5
dhamodarankms <dhamodarankms@gmail.com>
2023-05-18 09:52:33 +1200

    Apply infra changes #1

--- infra/AWS/set_up_aws_s3_bucket.tf
+++ infra/AWS/set_up_aws_s3_bucket.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "DataAnalytics" {
-    bucket = "sdgdatalakesecond"
+    bucket = "sdgdatalake"
     tags = {
         name = "SDG"
         value = "databricks"
