b832884f6a73c0c6a23c1e7eb6eff9b0baa2aac0
dhamodarankms <dhamodarankms@gmail.com>
2023-05-18 16:31:24 +1200

    infra changes AWS CLI

--- infra/AWS/set_up_aws_s3_bucket.tf
+++ infra/AWS/set_up_aws_s3_bucket.tf
@@ -1,13 +1,12 @@
 provider "aws" {
-    access_key = var.access_key
-    secret_key = var.secret_key
+    profile = "default"
     region = "ap-southeast-2"
 }
 
 resource "aws_s3_bucket" "DataAnalytics" {
-    bucket = "sdgdatalake"
+    bucket = "devdatalake"
     tags = {
         name = "SDG"
-        value = "databricks"
+        value = "AWS S3 Bucket"
     }
 }
